d63ceac75dcb6072c7fb21829f430705d2b3291d
shubh3131 <79696910+shubh3131@users.noreply.github.com>
2023-04-23 23:15:51 +0530

    Add files via upload
    This script takes a file with all the suid binaries running in the target system and checks it against the list of SUID binaries listed on gtfobins (https://gtfobins.github.io/#+suid) to look for SUID binaries on the target system which can be used escalate our privileges in unix/linux environment. 
    
    The purpose of creating this script is to save time by automatically finding the exploitable SUID binaries without having to manually go through them in the target system.

---
+++ check_suid_binaries.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Author Name: Shubham Sareen
+# Date Created: 23/04/2023
+# Last Modified: 23/04/2023
+
+# Description:
+# This script takes a file with all the suid binaries running in the target system and checks it against the list of suid binaries 
+# listed on gtfobins which can be used to escalate our privileges in unix/linux environment. 
+# The purpose of creating this script is to save time by automatically finding the exploitable suid binaries without having to manually
+# go through them in the target system.  
+
+# Usage:
+# ./check_suid.sh
+
+# Note: a file containing all of the exploitable suid binaries was made by the list available on gtfobins to run against this script.
+
+# Future Changes: Can automate the checking against suid binaries available on gtfobins directly from the  web page.
+
+
+# CODE STARTS HERE:
+
+if [ ! -f suids.txt ];then
+        find / -type f -perm -u=s -ls 2>/dev/null >> suids.txt
+else
+	echo "The suids.txt file will be used to check for useful suid binaries to escalate privileges"
+	echo
+fi
+
+
+ 
+if [ -f programs.txt ];then
+	echo
+else
+	cat suids.txt | tr -s " " | cut -d " " -f 12 | awk -F '/' '{print $NF}' >> programs.txt
+fi
+
+readarray -t target_suids < programs.txt
+readarray -t gtfo_suids < gtfo-binaries.txt
+
+echo "The suid binaries which can be used to escalate privileges on the target program are: "
+echo
+
+for target in "${target_suids[@]}"; do
+        if echo "${gtfo_suids[@]}" | grep -qw "$target"; then
+                echo "$target"
+                echo
+        fi
+
+done
+
+
+exit 0
+
+
